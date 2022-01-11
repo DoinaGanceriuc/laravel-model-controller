233ca0b29bb77b8332bce7a584fcf76f5db79dae
DoinaGanceriuc <doinaganceriuc@gmail.com>
2022-01-11 18:47:26 +0100

    Bonus: add scss

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,78 +10,8 @@
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
         <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
+        <link rel="stylesheet" href="{{asset('css/app.css')}}">
 
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-            .container {
-                max-width: 1140px;
-                margin: 2rem auto;
-            }
-            .title {
-                text-align: center;
-            }
-            .movies {
-                display: flex;
-                flex-wrap: wrap
-            }
-            .movie_card {
-                width: 25%;
-                border: 1px solid #DFDFDF;
-                border-radius: 1rem;
-                padding: 1rem;
-                margin: 0.5rem;
-                text-align: center;
-
-            }
-        </style>
     </head>
     <body>
         @yield('content')
