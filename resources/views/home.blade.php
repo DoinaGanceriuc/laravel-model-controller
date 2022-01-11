39dcb44eed4d0f5c796bd3563c4ab8bd65be2201
DoinaGanceriuc <doinaganceriuc@gmail.com>
2022-01-11 15:41:14 +0100

    create layouts file

---
+++ resources/views/home.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="container">
+    <h1 class="title">Home page</h1>
+
+</div>
+
+
+
+@endsection
---
+++ resources/views/movies.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('content')
+
+<h1 class="title">Movies</h1>
+
+@endsection
