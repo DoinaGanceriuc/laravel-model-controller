719d39f131bf3ad2d6e2eca86b02651ae90314b6
DoinaGanceriuc <doinaganceriuc@gmail.com>
2022-01-11 15:49:50 +0100

    add movie route and controller

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
 class MovieController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('home');
+    }
+    public function movie()
+    {
+        return view('movies');
+    }
 }
